c703c81a26c09813216ff6d5370e1964f673629e
Gökhan Erdogan <goekhan.erdogan@swisscom.com>
2022-07-29 21:37:30 +0200

    Added Basic Authentication for Web Security Added Import to Web Security to Starter Added Spring Security for pom.xml

--- src/main/java/com/assesment/titan/CloudApplication.java
+++ src/main/java/com/assesment/titan/CloudApplication.java
@@ -1,9 +1,12 @@
 package com.assesment.titan;
 
+import com.assesment.titan.config.BasicAuthWebSecurityConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
+@Import(BasicAuthWebSecurityConfiguration.class)
 public class CloudApplication {
 
 	public static void main(String[] args) {
