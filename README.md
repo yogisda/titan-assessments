fd7f07f80489d91c30890ee5ebc1ba04eb78baf6
Gökhan Erdogan <goekhan.erdogan@swisscom.com>
2022-07-29 23:47:11 +0200

    Added Swagger for API Documentation

--- README.md
+++ README.md
@@ -1,3 +1,1 @@
-# titan-assessments
-# titans-assessment
-# titans-assessment
+# titan-assessment
--- src/main/java/com/assesment/titan/CloudApplication.java
+++ src/main/java/com/assesment/titan/CloudApplication.java
@@ -3,14 +3,26 @@ package com.assesment.titan;
 import com.assesment.titan.config.BasicAuthWebSecurityConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 @Import(BasicAuthWebSecurityConfiguration.class)
 public class CloudApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CloudApplication.class, args);
-	}
 
+	}
+	@Bean
+	public Docket productApi() {
+		return new Docket(DocumentationType.SWAGGER_2).select()
+				.apis(RequestHandlerSelectors.basePackage("com.assesment.titan")).build();
+
+	}
 }
